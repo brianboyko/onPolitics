a177e58ee3f605cdbf178dba03a53e692542ac99
Brian Boyko <brian.boyko@gmail.com>
2018-06-21 03:26:29 -0400

    Updates

--- path----b635b14aa4d246428aee.js
+++ path----b635b14aa4d246428aee.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{104:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Brian Boyko (On Politics)"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{115:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Brian Boyko (On Politics)"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----b635b14aa4d246428aee.js.map
